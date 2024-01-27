349323d9f60ff8e9fbc347d3a8bbea1ee7e1197b
topherwalker <git@christopherwalker.tech>
2024-01-27 00:26:23 +0000

    Test Fix: sumMultiples

--- test/exercise5.test.js
+++ test/exercise5.test.js
@@ -5,17 +5,17 @@ import {
 	isItPrime,
 	createMatrix,
 	areWeCovered,
-} from '../challenges/exercise4';
+} from '../challenges/exercise5';
 
 
 describe('sumMultiples', () => {
 	test('returns the sum of numbers that are a multiple of 3 or 5', () => {
-		expect(sumMultiples([1, 3, 7, 8, 5, 10])).toBe(8);
+		expect(sumMultiples([1, 3, 7, 8, 5])).toBe(8);
 		expect(sumMultiples([5, 3, 7, 8, 1, 10])).toBe(18);
-		expect(sumMultiples([5, 22, 654, 123, 65, 23, 40, 1])).toBe(847);
+		expect(sumMultiples([5, 22, 654, 123, 65, 23, 40, 1])).toBe(887);
 	});
 	test('returns 0 if there are no numbers that are multiples of 3 or 5 ', () => {
-		expect(sumMultiples([1, 2, 7, 8, 1, 3])).toBe(0);
+		expect(sumMultiples([1, 2, 7, 8, 1])).toBe(0);
 	});
 	test('returns 0 if passed an empty string ', () => {
 		expect(sumMultiples([])).toBe(0);
